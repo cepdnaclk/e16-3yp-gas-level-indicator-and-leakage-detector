a38e5a02cdf8df76de7ba061ab9f07bd55d30a26
sudamkalpage <58980965+sudamkalpage@users.noreply.github.com>
2021-02-01 22:15:47 +0530

    Delete run.py

--- backend/run.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-from config import PORT,HOST
-
-# start the server in main in 30 thread maxmimum works can work at time
-if __name__ == "__main__":
-    uvicorn.run("Server:app",host=HOST,port=PORT,reload=True,debug=True,workers=1)
