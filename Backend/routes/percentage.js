e907a229f52172a303c5b93ff04aa3fdaff4081b
sudamkalpage <58980965+sudamkalpage@users.noreply.github.com>
2021-05-25 20:54:19 +0530

    Update percentage.js

--- Backend/routes/percentage.js
+++ Backend/routes/percentage.js
@@ -18,10 +18,23 @@ Router.post('/',function(req,res){
       })
 })
 
+Router.post('/update',function(req,res){     
 
+    const device_id = req.body.device_id
+    const percentage = req.body.percentage
+    console.log(req.body)
 
-
-
+    connection.query("select device_id from devices where device_id = '"+device_id+"'",function (err, data) { //check if user already exists
+        if (err) return res.sendStatus(400)   //incorrect token in the json
+        if (!Object.keys(data).length) return res.sendStatus(400) //email doen't exists
+        
+        connection.query('UPDATE devices SET balance_percentage = ? WHERE device_id = ?', [req.body.percentage,req.body.device_id], (err, rows, fields) => {
+            if (!err) res.send(req.body)
+            else console.log(err)
+        })
+          
+      })
+})
 
 module.exports = Router
 
